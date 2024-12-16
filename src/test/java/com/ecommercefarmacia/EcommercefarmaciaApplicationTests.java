47b973b22b89827a46c5ff2aff1b33ec390b4ffa
4lineferreira <alineferreirasnsd@gmail.com>
2024-12-16 09:46:58 -0300

    feat: criação do projeto

---
+++ src/test/java/com/ecommercefarmacia/EcommercefarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercefarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercefarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
